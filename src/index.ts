67414bd4d589f3884a195f80fb00251206da1fa3
bachacode <85910843+bachacode@users.noreply.github.com>
2023-06-15 19:58:11 -0400

    feat: bot login to server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-import * as fs from 'fs'
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import 'dotenv/config'
 
-fs
+const token = process.env.token
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
