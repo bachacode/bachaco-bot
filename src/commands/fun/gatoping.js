cc791d489dbcab2f5667614e204a89db43435c73
bachacode <85910843+bachacode@users.noreply.github.com>
2023-06-15 22:34:08 -0400

    feat: joining voice channel

---
+++ src/commands/fun/gatoping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('gatoping')
+    .setDescription('Replies with GatoPong!'),
+  async execute (interaction) {
+    await interaction.reply('GatoPong!')
+  }
+}
---
+++ src/commands/music/gatojoin.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, ChannelType } = require('discord.js')
+const { joinVoiceChannel } = require('@discordjs/voice')
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('gatojoin')
+    .setDescription('Makes the bot join your current voice!')
+    .addChannelOption((option) => option
+      .setName('channel')
+      .setDescription('The channel to join')
+      .setRequired(true)
+      .addChannelTypes(ChannelType.GuildVoice)
+    ).toJSON(),
+  async execute (interaction) {
+    const voiceChannel = interaction.options.getChannel('channel')
+    const voiceConnection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: interaction.guildId,
+      adapterCreator: interaction.guild.voiceAdapterCreator
+    })
+    voiceConnection
+  }
+}
