db6f02d162ef5cf23317b859f6450c8a91140cc7
bachacode <85910843+bachacode@users.noreply.github.com>
2023-06-23 16:29:25 -0400

    feat: song skipping

--- src/commands/music/gatoskip.js
+++ src/commands/music/gatoskip.js
@@ -1,8 +1,25 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('gatoskip').setDescription('Replies with GatoPong!'),
-  async execute (interaction) {
-    await interaction.reply('GatoPong!');
+  data: new SlashCommandBuilder()
+  .setName('gatoskip')
+  .setDescription('Skips the current song!'),
+  async execute ({client, interaction}) {
+    const queue = client.player.nodes.get(interaction.guild);
+
+    if(!queue) {
+      await interaction.reply('No hay nada sonando elmio.');
+      return;
+    }
+    const currentSong = queue.current;
+
+    queue.skip();
+
+    await interaction.reply({
+      embeds: [
+        new EmbedBuilder().setDescription(`Skipped **${currentSong.title}**`)
+        .setThumbnail(currentSong.thumbnail)
+      ]
+    })
   }
 };
