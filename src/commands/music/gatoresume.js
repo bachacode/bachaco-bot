3b1e86eabad53c17a2daa6a6dbf88c769f1b66e2
bachacode <85910843+bachacode@users.noreply.github.com>
2023-06-29 16:33:44 -0400

    feat: gatoresumir

--- src/commands/music/gatoresume.js
+++ src/commands/music/gatoresume.js
@@ -1,8 +1,29 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { useQueue } = require('discord-player');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('gatoresume').setDescription('Replies with GatoPong!'),
-  async execute (interaction) {
-    await interaction.reply('GatoPong!');
+  data: new SlashCommandBuilder()
+    .setName('gatoresume')
+    .setDescription('gatoc reanuda la canción actual.'),
+  async execute ({ interaction }) {
+    // Revisa si hay una queue activa.
+    const queue = useQueue(interaction.guild.id);
+
+    const embed = new EmbedBuilder().setColor('DarkAqua');
+
+    if (!queue) return interaction.reply('No hay nada sonando elmio.');
+
+    if(!queue.node.isPaused()) {
+      return interaction.reply({ embeds:[
+      embed.setDescription('¡gatoc no esta pausado!')]
+    });
+    }
+    queue.node.setPaused(false);
+
+    await interaction.reply({
+      embeds:[ 
+        embed.setDescription('¡gatoc ha reanudado la cancion!')
+      ]
+    });
   }
 };
