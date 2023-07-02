cd63d340b93678642d6c781c170608727ba432a0
bachacode <85910843+bachacode@users.noreply.github.com>
2023-07-01 22:13:18 -0400

    stream library changed

--- src/commands/music/gatoqueue.js
+++ src/commands/music/gatoqueue.js
@@ -1,8 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
-
+const { useQueue } = require("discord-player");
 module.exports = {
   data: new SlashCommandBuilder().setName('gatoqueue').setDescription('Replies with GatoPong!'),
-  async execute (interaction) {
+  async execute ({ interaction }) {
+    const queue = useQueue(interaction.guild.id);
+    const tracks = queue.tracks.toArray(); //Converts the queue into a array of tracks
+    const currentTrack = queue.currentTrack; //Gets the current track being played
+    console.log(tracks)
     await interaction.reply('GatoPong!');
   }
 };
