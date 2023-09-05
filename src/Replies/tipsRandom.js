16d045e54f7478eee75d59bc14351a28ab80229f
bachacode <85910843+bachacode@users.noreply.github.com>
2023-09-05 19:12:14 -0400

    track hyperlink

---
+++ src/Replies/tipsRandom.js
@@ -0,0 +1,21 @@
+const tipsRandom = [
+    { request: 'marico', response: 'eres' },
+    { request: 'verga', response: 'comes' },
+    { request: 'gatoc', response: '<:gatoC:957421664738639872> 🍷' },
+    {
+      request: 'boku no hero',
+      response:
+        'https://cdn.discordapp.com/attachments/801954893437861918/958565875961716736/unknown.png'
+    },
+    {
+      request: 'mediocre',
+      response:
+        'https://cdn.discordapp.com/attachments/801954893437861918/958565875961716736/unknown.png'
+    },
+    { request: 'women', response: '☕' },
+    { request: 'contexto', response: 'https://i.ytimg.com/vi/x6aBFSJHslE/hqdefault.jpg' },
+    { request: 'litio', response: '🐒' }
+  ];
+  
+  module.exports = funnyReplies;
+  
