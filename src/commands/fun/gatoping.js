6afce54e42f12a915fd7c27dc4fc3af29e480eac
bachacode <85910843+bachacode@users.noreply.github.com>
2023-12-22 14:33:35 -0400

    pingpong command with ms

--- src/commands/fun/gatoping.js
+++ src/commands/fun/gatoping.js
@@ -2,13 +2,17 @@ const { SlashCommandBuilder } = require('discord.js');
 /** @typedef {import('discord.js').ChatInputCommandInteraction} ChatInputCommandInteraction */
 
 /** @type {SlashCommandBuilder} */
-const data = new SlashCommandBuilder().setName('gatoping').setDescription('Replies with GatoPong!');
+const data = new SlashCommandBuilder()
+    .setName('gatoping')
+    .setDescription('Responde con los milisegundos de respuesta');
 
 /**
  * @param {ChatInputCommandInteraction} interaction
  */
 const execute = async (interaction) => {
-    await interaction.reply('GatoPong!');
+    const clientLatency = interaction.client.ws.ping.toFixed(0);
+
+    await interaction.reply(`:ping_pong: GatoPong! en ${clientLatency}ms`);
 };
 
 module.exports = {
