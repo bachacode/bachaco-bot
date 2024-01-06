731a98431465a603cf5f16f20963bf96b3cf62f3
bachacode <85910843+bachacode@users.noreply.github.com>
2024-01-05 22:35:03 -0400

    delete tenor helper

--- src/helpers/getTenorGifs.js
+++
@@ -1,9 +0,0 @@
-// const needle = require('needle');
-// const tenor = process.env.TENOR_KEY;
-// const searchTerms = '9277925';
-// const url = `https://tenor.googleapis.com/v2/posts?ids=${searchTerms}&key=${tenor}`;
-// needle.get(url, async (err, res) => {
-//     const json = res.body;
-
-//     console.log(json.results[0]);
-// });
