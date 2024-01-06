bc31df12470912bd8e7e7107ca9850c6964abc5d
bachacode <85910843+bachacode@users.noreply.github.com>
2024-01-05 22:52:02 -0400

    Revert "pingpong command with ms"
    This reverts commit 6afce54e42f12a915fd7c27dc4fc3af29e480eac.

--- src/commands/fun/gatoping.js
+++ src/commands/fun/gatoping.js
@@ -2,17 +2,13 @@ const { SlashCommandBuilder } = require('discord.js');
 /** @typedef {import('discord.js').ChatInputCommandInteraction} ChatInputCommandInteraction */
 
 /** @type {SlashCommandBuilder} */
-const data = new SlashCommandBuilder()
-    .setName('gatoping')
-    .setDescription('Responde con los milisegundos de respuesta');
+const data = new SlashCommandBuilder().setName('gatoping').setDescription('Replies with GatoPong!');
 
 /**
  * @param {ChatInputCommandInteraction} interaction
  */
 const execute = async (interaction) => {
-    const clientLatency = interaction.client.ws.ping.toFixed(0);
-
-    await interaction.reply(`:ping_pong: GatoPong! en ${clientLatency}ms`);
+    await interaction.reply('GatoPong!');
 };
 
 module.exports = {
