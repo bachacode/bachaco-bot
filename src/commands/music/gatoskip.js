28355ef39ab574ad0954c611f3c7388a38b2ac99
bachacode <85910843+bachacode@users.noreply.github.com>
2024-01-08 16:25:41 -0400

    support for jsdoc for commands

--- src/commands/music/gatoskip.js
+++ src/commands/music/gatoskip.js
@@ -1,32 +1,41 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { useQueue } = require('discord-player');
+/** @typedef {import('discord.js').ChatInputCommandInteraction} ChatInputCommandInteraction */
+
+/** @type {SlashCommandBuilder} */
+const data = new SlashCommandBuilder()
+    .setName('gatoskip')
+    .setDescription('gatoc se salta la canción actual.');
+
+/**
+ * @param {ChatInputCommandInteraction} interaction
+ */
+const execute = async (interaction) => {
+    // Revisa si hay una queue activa.
+    const queue = useQueue(interaction.guild.id);
+    if (!queue) return interaction.reply('No hay nada sonando elmio.');
+
+    const currentSong = queue.currentTrack;
+    queue.node.skip();
+
+    try {
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setDescription(`Se ha saltado **${currentSong.title}**`)
+                    .setThumbnail(currentSong.thumbnail)
+                    .setColor('DarkAqua')
+            ]
+        });
+    } catch (error) {
+        console.error(error);
+        await interaction.editReply({
+            content: '¡Ha ocurrido un error al saltar la canción!\n'
+        });
+    }
+};
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('gatoskip')
-        .setDescription('gatoc se salta la canción actual.'),
-    async execute(interaction) {
-        // Revisa si hay una queue activa.
-        const queue = useQueue(interaction.guild.id);
-        if (!queue) return interaction.reply('No hay nada sonando elmio.');
-
-        const currentSong = queue.currentTrack;
-        queue.node.skip();
-
-        try {
-            await interaction.reply({
-                embeds: [
-                    new EmbedBuilder()
-                        .setDescription(`Se ha saltado **${currentSong.title}**`)
-                        .setThumbnail(currentSong.thumbnail)
-                        .setColor('DarkAqua')
-                ]
-            });
-        } catch (error) {
-            console.error(error);
-            await interaction.editReply({
-                content: '¡Ha ocurrido un error al saltar la canción!\n'
-            });
-        }
-    }
+    data,
+    execute
 };
