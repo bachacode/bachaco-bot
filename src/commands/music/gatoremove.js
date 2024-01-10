0f986939a1840a4d2d7bda77a3d9ccff1d274e98
bachacode <85910843+bachacode@users.noreply.github.com>
2024-01-10 18:07:55 -0400

    gatoremove command for removing songs from the queue

--- src/commands/music/gatoremove.js
+++ src/commands/music/gatoremove.js
@@ -5,9 +5,14 @@ const embedOptions = require('../../config/embedOptions');
 
 /** @type {SlashCommandBuilder} */
 const data = new SlashCommandBuilder()
-    .setName('gatoskip')
-    .setDescription('gatoc se salta la canción actual.');
-
+    .setName('gatoremove')
+    .setDescription('Quita una canción de la cola.')
+    .addNumberOption((option) => {
+        return option
+            .setName('position')
+            .setDescription('Número de la canción a quitar')
+            .setRequired(true);
+    });
 /**
  * @param {ChatInputCommandInteraction} interaction
  */
@@ -15,15 +20,15 @@ const execute = async (interaction) => {
     // Revisa si hay una queue activa.
     const queue = useQueue(interaction.guild.id);
     if (!queue) return interaction.reply('No hay nada sonando elmio.');
-
-    const currentSong = queue.currentTrack;
-    queue.node.skip();
+    const trackNumber = interaction.options.getNumber('position', true) - 1;
+    const track = queue.tracks.toArray()[trackNumber];
+    queue.removeTrack(trackNumber);
 
     await interaction.reply({
         embeds: [
             new EmbedBuilder()
-                .setDescription(`Se ha saltado **${currentSong.title}**`)
-                .setThumbnail(currentSong.thumbnail)
+                .setDescription(`Se ha removido **${track.title}** de la cola`)
+                .setThumbnail(track.thumbnail)
                 .setColor(embedOptions.colors.default)
         ]
     });
