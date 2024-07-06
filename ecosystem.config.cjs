eed284c44b6cac334fd415d25c4a8801bf0219cc
bachacode <85910843+bachacode@users.noreply.github.com>
2024-07-05 20:46:47 -0400

    fix: removed ffmpeg static and fixed ecosystem file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,38 +1,38 @@
 module.exports = {
     apps: [
         {
-            namespace: "bachaco-repo",
-            name: "bachaco-bot",
-            script: "apps/bot/src/app.js",
-            watch: ["apps/bot/src"],
-            ignore_watch: ["node_modules", "*.log", "\\.git"],
+            namespace: 'bachaco-repo',
+            name: 'bachaco-bot',
+            script: 'apps/bot/src/app.js',
+            watch: ['apps/bot/src'],
+            ignore_watch: ['node_modules', '*.log', '\\.git'],
             watch_delay: 1000,
             watch_options: {
-                followSymlinks: false,
+                followSymlinks: false
             },
             env_production: {
-                NODE_ENV: "production",
+                NODE_ENV: 'production'
             },
             env_development: {
-                NODE_ENV: "development",
-            },
+                NODE_ENV: 'development'
+            }
         },
         {
-            namespace: "bachaco-repo",
-            name: "bachaco-api",
-            script: "apps/api/app.js",
-            watch: ["apps/api/src"],
-            ignore_watch: ["node_modules", "*.log", "\\.git"],
+            namespace: 'bachaco-repo',
+            name: 'bachaco-api',
+            script: 'apps/api/src/app.js',
+            watch: ['apps/api/src'],
+            ignore_watch: ['node_modules', '*.log', '\\.git'],
             watch_delay: 1000,
             watch_options: {
-                followSymlinks: false,
+                followSymlinks: false
             },
             env_production: {
-                NODE_ENV: "production",
+                NODE_ENV: 'production'
             },
             env_development: {
-                NODE_ENV: "development",
-            },
-        },
-    ],
+                NODE_ENV: 'development'
+            }
+        }
+    ]
 };
